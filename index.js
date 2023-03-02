193be1eaa132c1a50c1bc02ac5cf25e28c1d399a
Samira Shajahan Borsha <57401425+Samira-Shajahan-Borsha@users.noreply.github.com>
2023-03-02 20:46:53 +0600

    api created and send data from server side

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const categories = require('./data/categories.json');
+const news = require('./data/news.json');
 
 app.get('/', (req, res) => {
     res.send('News API running');
@@ -14,6 +15,28 @@ app.get('/', (req, res) => {
 app.get('/news-categories', (req, res) => {
     res.send(categories);
 });
+
+app.get('/news', (req, res) => {
+    res.send(news);
+});
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '08') {
+        res.send(news);
+    }
+    else {
+        const selected_category = news.filter(n => n.category_id === id);
+        res.send(selected_category);
+    }
+});
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selected_news = news.find(n => n._id === id)
+    res.send(selected_news);
+    // console.log(req);
+})
 
 
 app.listen(port, () => {
